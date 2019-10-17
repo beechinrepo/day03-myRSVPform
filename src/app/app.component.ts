0e48299ef7c8f8d1c1daff7a61614e98008014eb
Carine Ng <48783403+carine18@users.noreply.github.com>
2019-10-17 11:21:07 +0800

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,30 @@
-import { Component } from '@angular/core';
-import { NgForm } from '@angular/forms';
 
-import { MaterialModule } from './materials.module';
+import { RSVP } from './model';  //Import interface into AppComponent to use in function
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
 
-  processForm(f: NgForm) {
-    console.info('processing form: ', f.value);
-  }
+export class AppComponent {
+  shareable = false;
+
+  addToList($event: RSVP) {
+    console.info('RSVP list: ', $event);  //Generate entire form values
+  };
+
+  ngOnInit(){
+    this.shareable = !!navigator['share'];
+  };
+
+  sharethejoy(){
+    navigator['share']({
+      title: 'Class Reunion',
+      text: 'RSVP Now!',
+      url: 'carine18.github.io/myapp3/',
+  })
+    .then(() => console.log('Successful share'))
+    .catch((error) => console.log('Error sharing', error));
+  };
 }
