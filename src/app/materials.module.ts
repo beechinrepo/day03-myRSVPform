5a7d8c71a836f4ab349cac1ea339f8acd22f4ca3
Carine Ng <48783403+carine18@users.noreply.github.com>
2019-10-17 11:20:14 +0800

    Update materials.module.ts

--- src/app/materials.module.ts
+++ src/app/materials.module.ts
@@ -2,7 +2,19 @@ import { NgModule } from '@angular/core';
 import {MatInputModule} from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-const MATERIAL = [ MatButtonModule, MatIconModule ];
+import {MatRadioModule} from '@angular/material/radio';
+import {MatSelectModule} from '@angular/material/select';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import { MatMomentDateModule } from '@angular/material-moment-adapter';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatSliderModule} from '@angular/material/slider';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import {MatFormFieldModule} from "@angular/material"
+
+const MATERIAL = [MatInputModule, MatButtonModule, MatIconModule, MatRadioModule, MatSelectModule, 
+    MatCheckboxModule, MatDatepickerModule, MatMomentDateModule, MatToolbarModule, MatSliderModule, 
+    FlexLayoutModule, MatFormFieldModule ];
 
 @NgModule({
 imports: MATERIAL,
@@ -10,3 +22,4 @@ exports: MATERIAL
 })
 
 export class MaterialModule { }
+
