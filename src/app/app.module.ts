0a978b5eeb3479fcf9a4e40fbcd37c46fc262ee5
Carine Ng <48783403+carine18@users.noreply.github.com>
2019-10-17 11:20:41 +0800

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+
 import { FormsModule } from '@angular/forms';
 import { MaterialModule } from './materials.module';
 
-import { AppComponent } from './app.component';
-import { FormComponent } from './components/form.component';
+import { AppComponent } from './app.component';      
+import { RsvpComponent } from './rsvp.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+//All components in app must be imported & declared in AppModule
+//Other modules must be imported
 @NgModule({
   declarations: [
     AppComponent,
-    FormComponent
+    RsvpComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    MaterialModule
+    MaterialModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
